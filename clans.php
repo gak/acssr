1c813f4c039c9e0317d526e939fdd6c1c6bf927a
Gerald Kaszuba <gak@gakman.com>
2006-07-17 08:51:25 +0000

    git-svn-id: file:///home/svn/acssr/trunk@18 553f2fa2-ae18-0410-963e-c74ff6ea9e9f

--- clans.php
+++ clans.php
@@ -8,6 +8,7 @@ htmlArticleStart();
 These are the registered clan ranks. To register your clan you must be a member of ACSSR. Clans only are shown on the list if they have <?=$minclanplayers?> members who have played in the last fortnight. The score is the sum of the best <?=$minclanplayers?> clan members of each clan. This list is updated every 15 minutes.
 <?
 htmlArticleStop();
+homeHeading('Ranks');
 include 'static/clans.htm';
 htmlStop();
 ?>
