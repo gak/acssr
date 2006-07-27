4c8f00750a7071b8937f653756afec6fb342d893
Gerald Kaszuba <gak@gakman.com>
2006-07-27 04:04:55 +0000

    yup
    git-svn-id: file:///home/svn/acssr/trunk@29 553f2fa2-ae18-0410-963e-c74ff6ea9e9f

--- conf_db.php
+++ conf_db.php
@@ -1,7 +1,6 @@
 <?
 
-#define("DB_SERVER", "norris.gakman.com");
-define("DB_SERVER", "127.0.0.1:3307");
+define("DB_SERVER", '127.0.0.1:3307');
 #define("DB_SERVER", "");
 define("DB_USERNAME", "acssr");
 define("DB_PASSWORD", "homeracssr");
